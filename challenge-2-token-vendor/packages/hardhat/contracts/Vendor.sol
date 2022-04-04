cbe806f002bdb17dc208a5dad8360e2af243beed
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-04 21:59:23 +0100

    challenge-2

---
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.4;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YourToken.sol";
+
+contract Vendor is Ownable {
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+
+  uint256 public constant tokensPerEth = 100;
+
+  YourToken public yourToken;
+
+  constructor(address tokenAddress) {
+    yourToken = YourToken(tokenAddress);
+  }
+
+  // ToDo: create a payable buyTokens() function:
+  function buyTokens() {
+    emit Buytokens(msg.sender, msg.value, tokensPerEth * msg.value);
+  }
+
+  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+
+  // ToDo: create a sellTokens() function:
+
+}
